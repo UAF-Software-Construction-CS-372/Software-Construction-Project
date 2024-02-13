1b1bfbc0ce32422df2c3167822e5d5885598d0b6
kaleb burris <kaleb.r.burris@gmail.com>
2024-02-12 16:33:38 -0900

    chore: Do testing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
-﻿import { createServer } from 'http';
+﻿var http = require('http');
+var fs = require('fs');
 
-createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(8080);
+http.createServer( function (req, res) {
+    fs.readFile('src/test.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+    });
+
+    fs.readFile('public/login.html', function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(10000);
